1d47780ddb78d0bcacf66201a0725c59388f4158
mairyy <21020125@vnu.edu.vn>
2023-11-26 23:06:09 +0700

    buy ticket demo

--- src/screens/TicketScreen.js
+++ src/screens/TicketScreen.js
@@ -1,20 +1,51 @@
 import * as React from "react";
-import { View, Text, TouchableOpacity, Button, StyleSheet, Alert, Dimensions, SafeAreaView, TextInput} from "react-native";
+import { View, Text, TouchableOpacity, Button, StyleSheet, Alert, SafeAreaView, TextInput} from "react-native";
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import { useNavigation } from "@react-navigation/native";
 import { DateTimePickerAndroid } from '@react-native-community/datetimepicker';
 
 import BottomButtonBar from "../components/NavigatorBottomBar";
+import { Modal } from "../components/Modal";
+import { ticketStyles } from "../styles/globalStyles";
 
 export default function TicketScreen() {
     const [text, onChangeText] = React.useState();
     const [number, onChangeNumber] = React.useState();
+    const [adultTicket, onChangeAdult] = React.useState(0);
+    const [childTicket, onChangeChild] = React.useState(0);
+    const price = 30000;
+
+    const navigation = useNavigation();
+    const handlePress = (buttonName) => {
+        navigation.navigate(buttonName)
+    };
+
+    const currentDate = new Date();
     const [date, setDate] = React.useState(new Date());
+    const [warn, setWarn] = React.useState(false);
+
+    const [isConfirmVisible, setConfirmVisible] = React.useState(false);
+    const [isHelpVisible, setHelpVisible] = React.useState(false);
+    const toggleModal = (typeOfModal) => {
+        if (typeOfModal == 'confirm') {
+            setConfirmVisible(!isConfirmVisible);
+        } else {
+            setHelpVisible(!isHelpVisible);
+        }
+    };
 
     const onChange = (event, selectedDate) => {
-        const currentDate = selectedDate;
-        setDate(currentDate);
+        const currentDate = new Date();
+        if (currentDate.getDate() <= selectedDate.getDate() &&
+            currentDate.getMonth() <= selectedDate.getMonth() &&
+            currentDate.getFullYear() <= selectedDate.getFullYear()) {
+            setDate(selectedDate);
+            setWarn(false);
+        } else {
+            setDate(currentDate);
+            setWarn(true);
+        }
     };
 
     const showMode = (currentMode) => {
@@ -30,128 +61,119 @@ export default function TicketScreen() {
         showMode('date');
     };
 
-    const showTimepicker = () => {
-        showMode('time');
-    };
-
-    const navigation = useNavigation();
-    const handlePress = (buttonName) => {
-        navigation.navigate(buttonName)
-    };
-
     const formatDate = (date) => {
-        return `${date.getDate()}/${date.getMonth() +
-          1}/${date.getFullYear()}`;
+        date = new Date(date);
+        return (`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`);
     };
 
-    const typeDate = () => {
-        this.myTextInput.clear();
+    const onPlusPress = (typeOfTicket) => {
+        if (typeOfTicket == 'adult') {
+            onChangeAdult(adultTicket + 1);
+        } else {
+            onChangeChild(childTicket + 1);
+        }
+    };
+
+    const onMinusPress = (typeOfTicket) => {
+        if (typeOfTicket == 'adult') {
+            if (adultTicket > 0)
+                onChangeAdult(adultTicket - 1);
+        } else {
+            if (childTicket > 0)
+                onChangeChild(childTicket - 1);
+        }
+    };
+
+    const confirm = () => {
+        Alert.alert('Xác nhận thông tin');
     };
 
     return (
-        <View style={styles.container}>
-            <View style={styles.title}>
-                <TouchableOpacity style={styles.icon} onPress={() => navigation.goBack()}>
+        <View style={ticketStyles.container}>
+            <View style={ticketStyles.title}>
+                <TouchableOpacity style={ticketStyles.icon} onPress={() => navigation.goBack()}>
                     <Ionicons name="chevron-back-circle-outline" size={40}>
                     </Ionicons>
                 </TouchableOpacity>
-                <Text style={styles.titleText}>Đặt vé</Text>
-                <TouchableOpacity style={styles.icon} onPress={() => Alert.alert('Hướng dẫn mua vé')}>
-                    <Ionicons style={styles.icon} name="information-circle-outline" size={40}>
+                <Text style={ticketStyles.titleText}>Đặt vé</Text>
+                <TouchableOpacity style={ticketStyles.icon} onPress={() => Alert.alert('Hướng dẫn mua vé')}>
+                    <Ionicons style={ticketStyles.icon} name="information-circle-outline" size={40}>
                     </Ionicons>
                 </TouchableOpacity>
             </View>
 
-            <SafeAreaView style={styles.inputContainer}>
-                <Text style={styles.label}>Họ và tên:</Text>
+            <SafeAreaView style={ticketStyles.formContainer}>
+                <Text style={ticketStyles.label}>Họ và tên:</Text>
                 <TextInput
-                    style={styles.input}
+                    style={ticketStyles.input}
                     onChangeText={onChangeText}
                     value={text}
                     placeholder='Nguyễn Văn A'/>
-                <Text style={styles.label}>Số điện thoại:</Text>
+                <Text style={ticketStyles.label}>Số điện thoại:</Text>
                 <TextInput
-                    style={styles.input}
+                    style={ticketStyles.input}
                     onChangeNumber={onChangeNumber}
                     value={number}
                     keyboardType="numeric"/>
-                <Text style={styles.label}>Ngày tham quan:</Text>
-                <TouchableOpacity style={styles.labelContainer} onPress={showDatepicker}>
-                    <TextInput
-                        style={styles.inputDate}
-                        onChange={setDate}
-                        value={formatDate(date)}
-                        keyboardType="numeric"
-                        placeholder='DD/MM/YYYY'
-                        ref= {this.myTextInput} />
-                    <FontAwesome style={styles.calendar} name="calendar" size={20}></FontAwesome>
+                <Text style={ticketStyles.label}>Ngày tham quan:</Text>
+                <TouchableOpacity style={ticketStyles.inputContainer} onPress={() => showDatepicker}>
+                    <Text style={ticketStyles.inputDate}>{formatDate(date)}</Text>
+                    <FontAwesome style={ticketStyles.calendar} name="calendar" size={20}></FontAwesome>
                 </TouchableOpacity>
+                <View style={ticketStyles.warning}>
+                    {(warn) ? <Text style= {{color: 'red'}}>Vui lòng chọn ngày từ {formatDate(new Date())} trở đi!</Text> : <Text></Text>}
+                </View>
+                <View>
+                    <Text style={ticketStyles.label}>Số lượng vé:</Text>
+                    <View style={ticketStyles.ticketContainer}>
+                        <Text style={ticketStyles.subTitle}>Vé người lớn: </Text>
+                        <View style={ticketStyles.countContainer}>
+                            <TouchableOpacity onPress={() => onMinusPress('adult')}>
+                                <FontAwesome style={ticketStyles.countIcon} name="minus-square-o" size={18}></FontAwesome>
+                            </TouchableOpacity>
+                            <Text style={ticketStyles.countNumber}>{adultTicket}</Text>
+                            <TouchableOpacity onPress={() => onPlusPress('adult')}>
+                                <FontAwesome style={ticketStyles.countIcon} name="plus-square-o" size={18}></FontAwesome>
+                            </TouchableOpacity>
+                            <Text style={ticketStyles.priceStyle}>{price * adultTicket} VND</Text>
+                        </View>
+
+                    </View>
+                    <View style={ticketStyles.ticketContainer}>
+                        <Text style={ticketStyles.subTitle}>Vé học sinh / sinh viên / người cao tuổi: </Text>
+                        <View style={ticketStyles.countContainer}>
+                            <TouchableOpacity onPress={() => onMinusPress('child')}>
+                                <FontAwesome style={ticketStyles.countIcon} name="minus-square-o" size={18}></FontAwesome>
+                            </TouchableOpacity>
+                            <Text style={ticketStyles.countNumber}>{childTicket}</Text>
+                            <TouchableOpacity onPress={() => onPlusPress('child')}>
+                                <FontAwesome style={ticketStyles.countIcon} name="plus-square-o" size={18}></FontAwesome>
+                            </TouchableOpacity>
+                            <Text style={ticketStyles.priceStyle}>{price * childTicket / 2} VND</Text>
+                        </View>
+                    </View>
+                </View>
             </SafeAreaView>
+
+            <TouchableOpacity onPress={() => toggleModal('confirm')}>
+                <Text style={ticketStyles.button}>Xác nhận</Text>
+            </TouchableOpacity>
+
             <BottomButtonBar />
+            <Modal isVisible={isConfirmVisible}>
+                <Modal.Container>
+                    <Modal.Header title="Xác nhận thông tin" />
+                    <Modal.Body>
+                        <Text style={ticketStyles.subTitle}>Người đặt vé: {text}</Text>
+                        <Text style={ticketStyles.subTitle}>Số điện thoại: {number}</Text>
+                        <Text style={ticketStyles.subTitle}>Thông tin vé: {adultTicket} vé người lớn + {childTicket} vé học sinh /
+                         sinh viên / người cao tuổi, ngày {formatDate(date)}</Text>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button title="Xác nhận" onPress={() => toggleModal('confirm')} />
+                    </Modal.Footer>
+                </Modal.Container>
+            </Modal>
         </View>
     )
-}
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'flex-start',
-        position: 'relative',
-    },
-    title: {
-        flexDirection: 'row',
-        height: Dimensions.get('window').height * 0.08,
-        width: Dimensions.get('window').width * 0.98,
-        marginTop: 60,
-        marginLeft: 5,
-        justifyContent: 'space-between',
-        textAlign: 'center',
-        borderColor: 'grey',
-        borderWidth: 0,
-        borderBottomWidth: 2,
-    },
-    titleText: {
-        flex: 2,
-        color: "black",
-        fontSize: 30,
-        fontWeight: "bold",
-        textAlign: 'center',
-    },
-    icon: {
-        textAlign: 'center',
-        marginLeft: 20,
-        flex: 1,
-    },
-    inputContainer: {
-        flexDirection: 'column',
-        marginTop: 30,
-    },
-    input: {
-        height: 40,
-        margin: 12,
-        borderWidth: 1,
-        borderRadius: 8,
-        padding: 10,
-    },
-    label: {
-        marginLeft: 20,
-        fontWeight: "bold",
-        fontSize: 15,
-    },
-    labelContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    calendar: {
-        flex: 1,
-        marginTop: 20,
-    },
-    inputDate: {
-        height: 40,
-        margin: 12,
-        borderWidth: 1,
-        borderRadius: 8,
-        padding: 10,
-        flex: 8,
-    },
-})
+};
--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
 export const globalStyles = StyleSheet.create({
     container: {
@@ -79,4 +79,108 @@ export const FontFamily = {
     br_5xs: 8,
     br_81xl: 100,
   };
-  
+
+/* styles for ticket screens */
+export const ticketStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-start',
+        position: 'relative',
+    },
+    title: {
+        flexDirection: 'row',
+        height: Dimensions.get('window').height * 0.08,
+        width: Dimensions.get('window').width * 0.98,
+        marginTop: 60,
+        marginLeft: 5,
+        justifyContent: 'space-between',
+        textAlign: 'center',
+        borderColor: 'grey',
+        borderWidth: 0,
+        borderBottomWidth: 2,
+    },
+    titleText: {
+        flex: 2,
+        color: "black",
+        fontSize: 30,
+        fontWeight: "bold",
+        textAlign: 'center',
+    },
+    icon: {
+        textAlign: 'center',
+        marginLeft: 20,
+        flex: 1,
+    },
+    formContainer: {
+        flexDirection: 'column',
+        marginTop: 30,
+    },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        borderRadius: 8,
+        padding: 10,
+    },
+    label: {
+        marginLeft: 20,
+        fontWeight: "bold",
+        fontSize: 15,
+    },
+    inputContainer: {
+        flexDirection: 'row',
+    },
+    calendar: {
+        flex: 1,
+        marginTop: 20,
+    },
+    inputDate: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        borderRadius: 8,
+        padding: 10,
+        flex: 8,
+    },
+    countContainer: {
+        flexDirection: 'row',
+        flex: 3,
+    },
+    countIcon: {
+        marginLeft: 8,
+        marginTop: 3,
+    },
+    countNumber: {
+        marginLeft: 8,
+        fontSize: 15,
+    },
+    warning: {
+        marginLeft: 20,
+        paddingBottom: 5,
+        fontSize: 15,
+    },
+    ticketContainer: {
+        flexDirection: 'row',
+        paddingTop: 10,
+        paddingLeft: 10,
+    },
+    subTitle: {
+        flex: 5,
+    },
+    priceStyle: {
+        marginLeft: 8,
+        fontSize: 15,
+        fontStyle: 'italic',
+    },
+    button: {
+        width: 100,
+        height: 50,
+        textAlign: 'center',
+        fontSize: 20,
+        borderWidth: 2,
+        borderRadius: 8,
+        fontWeight: 'bold',
+        paddingTop: 10,
+        margin: Dimensions.get('window').width * 0.38,
+    },
+})
